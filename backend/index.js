79f644add41eed7142795936ce5dda62906e0426
Octane-dev <flxmedzn@gmail.com>
2025-03-23 23:08:34 +0000

    Debug for discord auth

--- backend/index.js
+++ backend/index.js
@@ -31,7 +31,7 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     cookie: { 
-        secure: process.env.NODE_ENV === 'production',
+        secure: true,
         httpOnly: true,
         sameSite: 'none'
     }
